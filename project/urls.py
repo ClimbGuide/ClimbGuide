183fbc2dd8e478158e096416e824f98b814c0d08
BelongaRobert <68036456+BelongaRobert@users.noreply.github.com>
2020-10-12 09:45:46 -0400

    base and home templates created

--- project/urls.py
+++ project/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.urls import include, path
-
+from climbguide import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home.html')
 ]
 
 if settings.DEBUG:
