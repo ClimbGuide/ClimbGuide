c23134a068ab103ecae3f10c7f95c9b7f3565786
BelongaRobert <68036456+BelongaRobert@users.noreply.github.com>
2020-10-12 15:16:23 -0400

    mapbox visible

--- climbguide/views.py
+++ climbguide/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    mapbox_access_token = 'pk.eyJ1IjoiYmVsb25nYXJvYmVydCIsImEiOiJja2c2cWd2N3IwdGluMnBwaWV5ZzU2bjhnIn0.QgRdSLNmSGfcu1CMWF7vhw'
+    return render(request, "home.html",
+    {'mapbox_access_token': mapbox_access_token })
