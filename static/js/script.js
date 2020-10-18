b4ebef73c5e084bb2c3dac6aee3372b0994751da
shaneneiman <shane.neiman@gmail.com>
2020-10-18 15:44:56 -0400

    refactor JS file and event ids for less repetitve coding

--- static/js/script.js
+++ static/js/script.js
@@ -1,49 +1,37 @@
 // Event Handlers
-function showDayTripForm () {
-    let dayTripForm = document.querySelector("#daytrip-form")
-    let editDayTripButton = document.querySelector("#edit-daytrip-button")
-    dayTripForm.classList.remove("hidden")
-    editDayTripButton.classList.add("hidden")
+function showForm () {
+    let formOne = document.querySelector("#form-one")
+    let buttonOne = document.querySelector("#button-one")
+    formOne.classList.remove("hidden")
+    buttonOne.classList.add("hidden")
 }
 
-function showUserForm () {
-    let userForm = document.querySelector("#user-form")
-    let editUserButton = document.querySelector("#edit-user-button")
-    userForm.classList.remove("hidden")
-    editUserButton.classList.add("hidden")
+function showFormTwo () {
+    let formTwo = document.querySelector("#form-two")
+    let buttonTwo = document.querySelector("#button-two")
+    formTwo.classList.remove("hidden")
+    buttonTwo.classList.add("hidden")
 }
 
-function showAddDayTripForm () {
-    let addDayTripForm = document.querySelector("#add-daytrip-form")
-    let addDayTripButton = document.querySelector("#add-daytrip-button")
-    addDayTripForm.classList.remove("hidden")
-    addDayTripButton.classList.add("hidden")
-}
-
-function showAddRoutePhotoForm () {
-    let addRoutePhotoForm = document.querySelector("#add-routephoto-form")
-    let addRoutePhotoButton = document.querySelector("#add-routephoto-button")
-    addRoutePhotoForm.classList.remove("hidden")
-    addRoutePhotoButton.classList.add("hidden")
+function showFormThree () {
+    let formThree = document.querySelector("#form-three")
+    let buttonThree = document.querySelector("#button-three")
+    formThree.classList.remove("hidden")
+    buttonThree.classList.add("hidden")
 }
 
 // Event Listeners
-let editDayTrip = document.querySelector("#edit-daytrip-button")
-if (editDayTrip) {
-    editDayTrip.addEventListener("click", showDayTripForm)
+let buttonOne = document.querySelector("#button-one")
+if (buttonOne) {
+    buttonOne.addEventListener("click", showForm)
 }
 
-let editUser = document.querySelector("#edit-user-button")
-if (editUser) {
-    editUser.addEventListener("click", showUserForm)
+let buttonTwo = document.querySelector("#button-two")
+if (buttonTwo) {
+    buttonTwo.addEventListener("click", showFormTwo)
 }
 
-let addDayTrip = document.querySelector("#add-daytrip-button")
-if (addDayTrip) {
-    addDayTrip.addEventListener("click", showAddDayTripForm)
-}
-
-let addRoutePhoto = document.querySelector("#add-routephoto-button")
-if (addRoutePhoto) {
-    addRoutePhoto.addEventListener("click", showAddRoutePhotoForm)
+let buttonThree = document.querySelector("#button-three")
+if (buttonThree) {
+    buttonThree.addEventListener("click", showFormThree)
 }
